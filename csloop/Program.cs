fb22ee510249bbe4144b0892a5bdd5816aec63fd
ohyeeune <s2018s33@e-mirim.hs.kr>
2020-05-08 12:02:41 +0900

    틱을 사용한 cpu 성능 측정하기 예제

--- csloop/Program.cs
+++ csloop/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,13 @@ namespace csloop
     {
         static void Main(string[] args)
         {
+            long start = DateTime.Now.Ticks;
+            long count = 0;
+            while (start + (10000000) > DateTime.Now.Ticks)
+            {
+                count++;
+            }
+            Console.WriteLine(count + "만큼 반복함");
         }
     }
 }
